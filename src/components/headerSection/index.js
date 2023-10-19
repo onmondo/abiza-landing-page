5993ff92293196e90f7c6e371996563ea2864dcf
John Raymond <john.raymond.blando@gmail.com>
2023-10-19 16:29:34 +0800

    Added display of homestay

--- src/components/headerSection/index.js
+++ src/components/headerSection/index.js
@@ -1,14 +1,35 @@
 import React, { Component } from "react";
+import beachImage from '../../assets/background-one.jpg'
+import logo from '../../assets/logo-transparent.png';
+// import mouse from '../../assets/mouse-scroll.svg';
+import homestay from '../../assets/homestay.png';
 
 class HeaderSection extends Component {
     render() {
         return (
-            <section id="aboutme">
-                <header>
-                    <h1>Discover tranquility at Abiza Homestay</h1>
-                    <p>Disconnect from the bustle, connect with tranquility.</p>
-                </header>
-            </section>
+            <>
+                <article id="aboutme">
+                    <header>
+                        <h1>Discover tranquility at Abiza Homestay</h1>
+                        <p className="description">Disconnect from the bustle, connect with tranquility.</p>
+                    </header>
+                </article>
+                <section id="promote">
+                    <section className="homestayreview">
+                        <img className="profilepic" src={homestay} loading="lazy"></img>
+                        <header>
+                            <h4>Abiza Homestay</h4>
+                            <p className="description">Starts from Php400</p>
+                        </header>
+                    </section>
+                    <img className="backgroundimg" src={beachImage} loading="lazy"></img>
+                    <span id="promotetop"></span>
+                    <span id="promotebottom">
+                        <img src={logo} loading="lazy"></img>
+                    </span>
+                </section>
+            </>
+
         )
     }
 }
