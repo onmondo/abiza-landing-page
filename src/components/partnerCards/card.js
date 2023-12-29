80a60ca77ba294da35383bb0c4f57575cefc243c
John Raymond <john.raymond.blando@gmail.com>
2023-12-29 15:45:36 +0800

    Refactor partners page components

---
+++ src/components/partnerCards/card.js
@@ -0,0 +1,40 @@
+import React from "react";
+import QRCode from "react-qr-code";
+import oneInNewWindowIcon from '../../assets/icons8-open-in-new-window-25.png';
+
+function PartnerCard(props) {
+    const { partner } = props
+    return (
+        <>
+            <dt>Logo</dt>
+            <dd><img src={partner.logo} loading="lazy" alt={partner.description} /></dd>
+            <dt>Link</dt>
+            <dd>
+                <a href={partner.url} target="blank">
+                    {partner.description}
+                    <img src={oneInNewWindowIcon} alt="Open in new window icon" />
+                </a>
+            </dd>
+            <dt>QR Code</dt>
+            <dd>
+                <a href={partner.url} target="blank">
+                    <QRCode value={partner.url} />
+                </a>
+            </dd>
+            <dt>Price</dt>
+            <dd className="price">
+                <blockquote>
+                    {partner.currency.symbol}{partner.price}<aside>.00{partner.currency.name}</aside>
+                </blockquote>
+            </dd>
+            <dt className="extra">After 2 guests, per night</dt>
+            <dd className="extra">
+                <blockquote>
+                    {partner.currency.symbol}{partner.guests.extra.price}<aside>.00{partner.currency.name}</aside>
+                </blockquote>
+            </dd>
+        </>
+    )
+}
+
+export default PartnerCard;
--- src/components/partnerCards/index.js
+++ src/components/partnerCards/index.js
@@ -1,6 +1,5 @@
 import React from "react";
-import QRCode from "react-qr-code";
-import oneInNewWindowIcon from '../../assets/icons8-open-in-new-window-25.png';
+import PartnerCard from "./card";
 
 function PartnerCards(props) {
     const { partners } = props
@@ -10,33 +9,7 @@ function PartnerCards(props) {
             {partners.map((partner, index) => {
                 return (
                     <dl key={index}>
-                        <dt>Logo</dt>
-                        <dd><img src={partner.logo} loading="lazy" alt={partner.description} /></dd>
-                        <dt>Link</dt>
-                        <dd>
-                            <a href={partner.url} target="blank">
-                                {partner.description}
-                                <img src={oneInNewWindowIcon} alt="Open in new window icon" />
-                            </a>
-                        </dd>
-                        <dt>QR Code</dt>
-                        <dd>
-                            <a href={partner.url} target="blank">
-                                <QRCode value={partner.url} />
-                            </a>
-                        </dd>
-                        <dt>Price</dt>
-                        <dd className="price">
-                            <blockquote>
-                                {partner.currency.symbol}{partner.price}<aside>.00{partner.currency.name}</aside>
-                            </blockquote>
-                        </dd>
-                        <dt className="extra">After 2 guests, per night</dt>
-                        <dd className="extra">
-                            <blockquote>
-                                {partner.currency.symbol}{partner.guests.extra.price}<aside>.00{partner.currency.name}</aside>
-                            </blockquote>
-                        </dd>
+                        <PartnerCard partner={partner} />
                     </dl>
                 )
             })}
